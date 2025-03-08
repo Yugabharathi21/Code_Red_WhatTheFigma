9f86bfcf19da70fc61f825ccbc55991d303fcbe5
Yuga Bharathi Jaisankar <102886591+Yugabharathi21@users.noreply.github.com>
2025-03-09 03:20:04 +0530

    feat: refactor App component to conditionally render Navigation and Footer based on route (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,23 +9,32 @@ import ProductDetail from './pages/ProductDetail';
 import Login from './pages/Login';
 import Article from './pages/Article';
 
+const AppContent = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      {!isLoginPage && <Navigation />}
+      <main className={`flex-1 ${isLoginPage ? '' : ''}`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product/1" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+      </main>
+      {!isLoginPage && <Footer />}
+    </div>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navigation />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/product/1" element={<ProductDetail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/article" element={<Article />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
